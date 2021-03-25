164eb14aedeb94c1660502bf439bbb417916977b
Karl Yamashita <344712+karlyamashita@users.noreply.github.com>
2021-03-25 11:03:23 -0700

    Renamed functions

--- TimerCallback.h
+++ TimerCallback.h
@@ -3,26 +3,34 @@
 
 #include "main.h"
 
+#define MAX_TIMER_CALLBACK 8 // increase if more callbacks are needed
 
 enum TIMER_CALLBACK_RATE{
 	NO_REPEAT_TIMER,
 	REPEAT_TIMER
 };
 
+typedef struct TimerCallbackStruct{
+    TimerCallback callback;// what function to callback
+    uint8_t timerRepeat; // repeat or disable after callback
+    uint8_t timerEnabled; // enable/disable callback
+    uint32_t timerValue; // the time to reach
+    uint32_t timerCount; // the current timer count
+}TimerCallbackStruct;
+
 typedef void (*TimerCallback)(void);// no data is passed
 
-int8_t CreateTimerCallback(TimerCallback callback, uint32_t timerCountThreshold, uint8_t repeat);
-uint8_t ClearTimerCallbackTimer(TimerCallback callback);
-uint8_t RemoveTimerCallback(TimerCallback callback);
-uint8_t EnableTimerCallback(TimerCallback callback, uint8_t mode) ;
-uint8_t GetTimerCallbackEnableStatus(TimerCallback callback, uint8_t *status);
-uint8_t GetTimerCallbackCurrentTimerValue(TimerCallback callback, uint32_t *timerValue);
-uint8_t SetTimerCallbackTimerValue(TimerCallback callback, uint32_t timerValue);
-uint8_t SetCallbackTimerThresholdValue(TimerCallback callback, uint32_t timerThresholdValue, uint8_t repeat);
-uint8_t GetTimerCallbackExists(TimerCallback callback);
-void IncrementTimerCallback(void);
-void CheckTimerCallback(void);
-void SortTimerCallback(void);
+int8_t TimerCallbackRegister(TimerCallback callback, uint32_t timerCountThreshold, uint8_t repeat);
+uint8_t TimerCallbackClearTimer(TimerCallback callback);
+uint8_t TimerCallbackDelete(TimerCallback callback);
+uint8_t TimerCallbackEnable(TimerCallback callback, uint8_t mode) ;
+uint8_t TimerCallbackEnableStatus(TimerCallback callback, uint8_t *status);
+uint8_t TimerCallbackGetCurrentTimerValue(TimerCallback callback, uint32_t *timerValue);
+uint8_t TimerCallbackSetTimerRepeat(TimerCallback callback, uint32_t timerThresholdValue, uint8_t repeat);
+uint8_t TimerCallbackExists(TimerCallback callback);
+void TimerCallbackIncrement(void);
+void TimerCallbackCheck(void);
+static void TimerCallbackSort(void);
 
 
 
