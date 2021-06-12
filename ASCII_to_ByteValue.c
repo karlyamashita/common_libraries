9739f832c27edf4361db278bff4b69fd175acf31
Karl Yamashita <karlyamashita@gmail.com>
2021-06-11 19:46:04 -0700

    Added ASCII_to_ByteValue and I2C_Buffer files. I2C_Buffer files is for TI, not ST.

---
+++ ASCII_to_ByteValue.c
@@ -0,0 +1,34 @@
+/*
+ * ASCII_to_ByteValue.c
+ *
+ *  Created on: Jun 11, 2021
+ *      Author: karl.yamashita
+ */
+
+#include "main.h"
+#include "ASCII_to_ByteValue.h"
+
+
+/*
+ * Description: Get byte value of a single character specifically for CMDx where x can equal 0-9 and A-Z
+ *              A = 10, B = 11...Y = 34, Z = 35.
+ *
+ * Input: ASCII character
+ * Output: integer value of character
+ *
+ */
+int GetASCII_ByteValue(char chr){
+    int msgPointer = 0;
+    if(chr >= 0x30 && chr <= 0x39){
+        msgPointer = chr - 0x30;
+    }
+    else if(chr >= 0x41 && chr <= 0x5A){
+        msgPointer = (chr - 0x40) + 9;
+    }
+    else
+    {
+        return -1;
+    }
+    return msgPointer;
+}
+
---
+++ ASCII_to_ByteValue.h
@@ -0,0 +1,15 @@
+/*
+ * ASCII_to_ByteValue.h
+ *
+ *  Created on: Jun 11, 2021
+ *      Author: karl.yamashita
+ */
+
+#ifndef ASCII_TO_BYTEVALUE_H_
+#define ASCII_TO_BYTEVALUE_H_
+
+
+int GetASCII_ByteValue(char chr);
+
+
+#endif /* ASCII_TO_BYTEVALUE_H_ */
