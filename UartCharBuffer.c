8f243194db4d184a16d065faed3021da69dd4d5c
Karl Yamashita <karlyamashita@gmail.com>
2022-11-20 12:04:44 -0800

    Updated to receive from two UART ports. This has not been validated yet! I have more modifications from another computer that I need to merge with this one before I can validate.

--- UartCharBuffer.c
+++ UartCharBuffer.c
@@ -1,30 +1,29 @@
 /*
-  	  UartCharBuffer.c
-
-   Created on: Dec 2, 2019
-       Author: Karl
-
-       06-16-2021 Rev. 1.0.1 - Changed code to make more universal.
-       10-19-2022 Rev. 1.0.2 - Can now send bytes instead of ASCII only
-
-       // Basic Info
-       • This is a universal uart message buffer for ASCII characters.
-       • It has a ring buffer for individual characters and a ring buffer for messages.
-       • This file shouldn't need any modifications except for bug fixes.
-
-       // Receive info
-       • Use AddUartCharBuffer() to add single characters to a character buffer. You would usually call this from a uart IRQ.
-       • Call UartParseRxCharBuffer() from a polling routine.
-          This will parse the character buffer, looking for a '\r' to then save the characters to a message buffer.
-       • Use ParseUartRxMessageBuffer() as an example to parse the message buffer. Write you own code inside the function.
-
-       // Transmit info
-       • Use UartAddTxMessageBuffer() to add a message to a Tx message buffer.
-       • Call UartSendMessage() from a polling routine. This will send any messages in the transmit message buffer.
-
-
-
-       Note1: Adjust value in UartCharBuffer.h file accordingly.
+ * UartCharBuffer.c
+ *
+ *  Created on: Dec 2, 2019
+ *      Author: Karl
+ *
+ *      06-16-2021 Rev. 1.0.1 - Changed code to make more universal.
+ *
+ *      // Basic Info
+ *      � This is a universal uart message buffer for ASCII characters. It has a ring buffer for individual characters and a ring buffer for messages.
+ *      � Do not use this for 8bit data. Receiving 8bit data requires a different type of buffer and parser.
+ *      � This file shouldn't need any modifications except for bug fixes.
+ *
+ *      // Receive info
+ *      � Use AddUartCharBuffer() to add single characters to a character buffer. You would usually call this from a uart IRQ.
+ *      � Call UartParseRxCharBuffer() from a polling routine.
+ *         This will parse the character buffer, looking for a '\r' to then save the characters to a message buffer.
+ *      � Use ParseUartRxMessageBuffer() as an example to parse the message buffer. Write you own code inside the function.
+ *
+ *      // Transmit info
+ *      � Use UartAddTxMessageBuffer() to add a message to a Tx message buffer.
+ *      � Call UartSendMessage() from a polling routine. This will send any messages in the transmit message buffer.
+ *
+ *
+ *
+ *      Note1: Adjust value in UartCharBuffer.h file accordingly.
         #define MAX_UART_RX_CHAR_BUFFER_SINGLE 1
         #define MAX_UART_RX_CHAR_BUFFER 192 // this should be double the size of data you are receiving up to '\r' character.
         #define MAX_UART_TX_CHAR_BUFFER 192
@@ -33,68 +32,127 @@
 
         Note2: These includes should be defined in main.h in these order
         #include "RingBuff.h"
+        #include "UartIncludes.h" // see Note1
         #include "UartCharBuffer.h"
  */
 
 #include "main.h"
 #include "UartCharBuffer.h"
 
-// this is the array that holds the data from the UART interrupt. For STM32 we only need one character
-uint8_t uartDataBuffer[MAX_UART_RX_CHAR_BUFFER_SINGLE] = {0};
 
-// Rx character buffer. The array for the Uart IRQ.
-uint8_t uartRxCharBuffer[MAX_UART_RX_CHAR_BUFFER];
-RING_BUFF_INFO uartRxCharBufferPointer;
+// the array for the UART IRQ data received.
+uint8_t uartIRQ_ByteBuffer[MAX_UART_RX_IRQ_BYTE_LENGTH];
+
+/*
+ * Description: I have a new project where I have to receive data on two UART ports where before it was only one.
+ * 				I am rewriting the code to accept from either port. So index 0 can be whatever port you assign to it,
+ * 				and index 1 is another port. It is up to the user to know that index 0 or 1 is what port your are receiving from.
+ * 				The data can be either ASCII or Binary on either index, but you can't have both ASCII and Binary on the same index.
+ * 				User will need to know which Parsing routine to use for that index, UartParseRx_BINARY_Buffer() or UartParseRx_CHAR_Buffer()
+ *
+ */
+// Declare the data buffer for two ports to accept data in ASCII or binary.
+UartRxByteBufferStruct uartRxByteBuffer[NUMBER_OF_UART_PORTS] = {0};
+RING_BUFF_INFO uartRxByteBufferPointer[NUMBER_OF_UART_PORTS] = {0};
+
 
 // Rx message buffer
-UartCharBufferRxStruct uartRxMessageBuffer[MAX_UART_RX_MESSAGE_BUFFER];
+UartRxMsgBufferStruct uartRxMessageBuffer[MAX_UART_RX_MESSAGE_BUFFER];
 RING_BUFF_INFO uartRxMsgBufferPointer;
 // Tx message buffer
-UartCharBufferTxStruct uartTxMessageBuffer[MAX_UART_TX_MESSAGE_BUFFER];
+UartTxMsgBufferStruct uartTxMessageBuffer[MAX_UART_TX_MESSAGE_BUFFER];
 RING_BUFF_INFO uartTxMsgBufferPointer;
 
 
 /*
- * Description: Parse uart character buffer and save in message buffer.
- *              This needs to be called from a polling routine
+ * Description: Parse UART character buffer and save in message buffer. The string being received should have LF terminator
+ *              This needs to be called from a polling routine.
  *
- * Input: none
+ * Input: Index pointer to which byte buffer to parse
  * Output: none
  */
-void UartParseRxCharBuffer(void){
+void UartParseRx_CHAR_Buffer(uint8_t index)
+{
     static uint32_t idxPtr = 0;
-    if (uartRxCharBufferPointer.iCnt_Handle) {
-        uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexIN].data[idxPtr++] = uartRxCharBuffer[uartRxCharBufferPointer.iIndexOUT]; // save character
-        if(uartRxCharBuffer[uartRxCharBufferPointer.iIndexOUT] == '\n'){ // check if carriage return
+    uint32_t portNumber;
+
+    switch(index)
+    {
+    case 0:
+    	portNumber = UART_INDEX_PTR_0;
+    	break;
+    case 1:
+    	portNumber = UART_INDEX_PTR_1;
+    	break;
+    	// add more if more than 2 UART ports
+    default:
+    	portNumber = UART_INDEX_PTR_0;
+    	break;
+    }
+
+    if (uartRxByteBufferPointer[index].iCnt_Handle) {
+        uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexIN].data[idxPtr++] = uartRxByteBuffer[index].data[uartRxByteBufferPointer[index].iIndexOUT]; // save character
+        if(uartRxByteBuffer[uartRxMsgBufferPointer.iIndexIN].data[uartRxByteBufferPointer[index].iIndexOUT] == '\n'){ // check if line feed
             idxPtr = 0;  // reset pointer
+            uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexIN].uartPort = portNumber;
+            uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexIN].dataSize = idxPtr - 1;
             DRV_RingBuffPtr__Input(&uartRxMsgBufferPointer, MAX_UART_RX_MESSAGE_BUFFER); // new message complete
         }
-        DRV_RingBuffPtr__Output(&uartRxCharBufferPointer, MAX_UART_RX_CHAR_BUFFER); // next character
+        DRV_RingBuffPtr__Output(&uartRxByteBufferPointer[index], MAX_UART_RX_BYTE_BUFFER); // next character
     }
 }
 
 /*
- * Description: Parse uart buffer for bytes and save in message buffer.
- * 				You'll need to know how many bytes to parse and/or up to the checksum.
- *              This needs to be called from a polling routine
+ * Description: Parse UART Binary buffer and save in message buffer.
+ *              This needs to be called from a polling routine.
  *
- * Input: none
+ * Input: Index pointer to which UART buffer to parse, the size of bytes to receive for a complete packet
  * Output: none
  */
-void UartParseRxByteBuffer(void){
-	// not implemented yet
+void UartParseRx_BINARY_Buffer(uint8_t index, uint32_t packetSize)
+{
+	static uint32_t idxPtr = 0;
+    uint32_t portNumber;
+
+    switch(index)
+    {
+    case 0:
+    	portNumber = UART_INDEX_PTR_0;
+    	break;
+    case 1:
+    	portNumber = UART_INDEX_PTR_1;
+    	break;
+    	// add more if more than 2 UART ports
+    default:
+    	portNumber = UART_INDEX_PTR_0;
+    	break;
+    }
+
+    if (uartRxByteBufferPointer[index].iCnt_Handle)
+    {
+		uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexIN].data[idxPtr++] = uartRxByteBuffer[index].data[uartRxByteBufferPointer[index].iIndexOUT]; // save character
+		if(idxPtr > packetSize)
+		{
+			if(ValidateChkSum(uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexIN].data, packetSize)) // validate checksum
+			{
+				DRV_RingBuffPtr__Input(&uartRxMsgBufferPointer, MAX_UART_RX_MESSAGE_BUFFER); // new message complete
+				uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexIN].uartPort = portNumber;
+				uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexIN].dataSize = packetSize;
+			}
+		}
+        DRV_RingBuffPtr__Output(&uartRxByteBufferPointer[index], MAX_UART_RX_BYTE_BUFFER); // next character
+    }
 }
 
-
 /*
- * Description: Add byte or char array to message buffer to be sent.
+ * Description: Add char array to message buffer to be sent.
  *              Be sure to call UartSendMessage() in a polling routine
  *
  * Input: The data array
  * Output:
  *
  */
-void UartAddTxMessageBuffer(UartCharBufferTxStruct *uartBufferPointer_IN){
+void UartAddTxMessageBuffer(UartTxMsgBufferStruct *uartBufferPointer_IN){
     uint8_t i = 0;
     uint8_t *pData = uartBufferPointer_IN->data;
 
@@ -105,11 +163,8 @@ void UartAddTxMessageBuffer(UartCharBufferTxStruct *uartBufferPointer_IN){
     memset(&uartTxMessageBuffer[uartTxMsgBufferPointer.iIndexIN].data, 0, sizeof(uartTxMessageBuffer[uartTxMsgBufferPointer.iIndexIN].data));
 
     uartTxMessageBuffer[uartTxMsgBufferPointer.iIndexIN].uartPort = uartBufferPointer_IN->uartPort;
-    uartTxMessageBuffer[uartTxMsgBufferPointer.iIndexIN].dataLength = uartBufferPointer_IN->dataLength;
-
-    for(i = 0; i < uartBufferPointer_IN->dataLength; i++)
-    {
-        uartTxMessageBuffer[uartTxMsgBufferPointer.iIndexIN].data[i] = *pData;
+    while(*pData != '\0'){
+        uartTxMessageBuffer[uartTxMsgBufferPointer.iIndexIN].data[i++] = *pData;
         pData++;
     }
 
@@ -119,7 +174,7 @@ void UartAddTxMessageBuffer(UartCharBufferTxStruct *uartBufferPointer_IN){
 
 /*
  * Description: Call this from a polling routine.
- *              UartTxMessage() This is a generic function and should be part of your code for your specific MCU.
+ *              UartTxMessage() should be part of your code for your specific MCU.
  *
  *              ********* Example function defined for STM32 *********
  *              HAL_StatusTypeDef UartTxMessage(UartCharBufferTxStruct *uartBufferPointer)
@@ -184,112 +239,100 @@ void UartAddTxMessageBuffer(UartCharBufferTxStruct *uartBufferPointer_IN){
  *
  */
 void UartSendMessage(void){
-    int status;
+    HAL_StatusTypeDef HAL_Status = HAL_OK;
     if (uartTxMsgBufferPointer.iCnt_Handle) {
-    	// UartTxMessage() is a generic call. You'll have to create a function specific to your MCU
-        status = UartTxMessage(&uartTxMessageBuffer[uartTxMsgBufferPointer.iIndexOUT]);
-        if (status == NO_ERROR) {
+        // TODO - pass argument which uart port to send on
+        HAL_Status = UartTxMessage(&uartTxMessageBuffer[uartTxMsgBufferPointer.iIndexOUT]);
+        if (HAL_Status == HAL_OK) {
             DRV_RingBuffPtr__Output(&uartTxMsgBufferPointer, MAX_UART_TX_MESSAGE_BUFFER);
+        }
+        else
+        {
+
         }
     }
 }
 
 
 /*
- * Description: add one byte or character to the character buffer.
+ * Description: Add one byte to the data buffer.
  *
- * Input: reference to char array. uartPort not currently used 
- * Return: Error status
+ * Input: index to which data buffer to write to, the pointer to data array, the size/length of data
+ * Output: No error or overflow
  */
-int UartAddCharToBuffer(uint8_t uartPort, uint8_t *_char_IN){
-    if(uartRxCharBufferPointer.iCnt_OverFlow){
-        // character buffer is full, return and let UartParseRxCharBuffer() routine parse character buffer to free some space.
-        return UART_BUSY;
+int UartAddByteToBuffer(uint8_t index, uint8_t *data, uint32_t sizeOfData){
+	int i;
+    if(uartRxByteBufferPointer[index].iCnt_OverFlow){
+        // byte buffer is full. You should monitor the return status during development and increase buffer size to if overflow;
+        return UART_BUFFER_OVERFLOW;
     }
-    uartRxCharBuffer[uartRxCharBufferPointer.iIndexIN] = *_char_IN;
-    DRV_RingBuffPtr__Input(&uartRxCharBufferPointer, MAX_UART_RX_CHAR_BUFFER);
+
+	for(i = 0; i < sizeOfData; i++)
+	{
+		uartRxByteBuffer[index].data[uartRxByteBufferPointer[index].iIndexIN] = *data;
+		DRV_RingBuffPtr__Input(&uartRxByteBufferPointer[index], MAX_UART_RX_BYTE_BUFFER);
+	}
 
     return NO_ERROR;
 }
 
 /*
- * Description: Helper to copy the uart port and string to a buffer structure
+ * Description: Helper to copy the uart port and data to a buffer structure
  *
- * Input: The Port number, the string to copy to array
- * Output: The UartCharBufferTxStruct transmit data structure
+ * Input: The Port number, the transmit data structure, the string to copy to array
+ * Output: none
  *
  */
-int UartCopyStrToCharBufferTxStruct(uint8_t uartPort, char *str_IN, UartCharBufferTxStruct *uartTx_OUT)
-{
-    uint8_t *pData = uartTx_OUT->data; // pointer to data buffer
-    uint32_t strLength = strlen(str_IN);
-
-    if(strlen(str_IN) == 0) return -1; // make sure there are some characters.
+int UartCopyStrToCharBufferTxStruct(uint8_t uartPort, UartTxMsgBufferStruct *uartTx_OUT, char *str_IN){
+    uint8_t *pData = uartTx_OUT->data; // pointer to data
+    if(strlen(str_IN) == 0) return -1; // make sure there is some characters.
     memset(uartTx_OUT, 0, sizeof(*uartTx_OUT)); // clear variable
     uartTx_OUT->uartPort = uartPort; // save uart port
-    uartTx_OUT->dataLength = strLength; // save the string length
-    memcpy(pData, (uint8_t*)str_IN, strLength); // copy str to data array.
-
+    memcpy(pData, (uint8_t*)str_IN, strlen(str_IN)); // copy str to data array.
     return 0;
 }
-
-/*
- * Description: Helper to copy the uart port and data array to a buffer structure
- *
- * Input: The Port number, the data array to copy to data buffer, the data length
- * Output: The UartCharBufferTxStruct transmit data structure
- *
- */
-int UartCopyDataToDataBufferTxStruct(uint8_t uartPort, uint8_t *data_IN, uint32_t dataLen, UartCharBufferTxStruct *uartTx_OUT)
-{
-	uint8_t *pData = uartTx_OUT->data; // pointer to data buffer
-
-	memset(uartTx_OUT, 0, sizeof(*uartTx_OUT)); // clear variable
-	uartTx_OUT->uartPort = uartPort; // save uart port
-	uartTx_OUT->dataLength = dataLen;
-	memcpy(pData, data_IN, dataLen); // copy data array to buffer array.
-
-	return 0;
-}
-
 
 /*
  * This is an example on how to parse a message from the message buffer. Copy this to a file and call this function from polling routine.
  *
  * You will need to define two extern variables where ever you have this function
- * • extern UartCharBufferRxStruct uartRxMessageBuffer[MAX_UART_RX_MESSAGE_BUFFER];
- * • extern RING_BUFF_INFO uartRxMsgBufferPointer;
+ * � extern UartCharBufferRxStruct uartRxMessageBuffer[MAX_UART_RX_MESSAGE_BUFFER];
+ * � extern RING_BUFF_INFO uartRxMsgBufferPointer;
  *
  */
 /*
 void ParseUartRxMessageBuffer(void){
+    UartCharBufferTxStruct uartTx;
     uint8_t message[MAX_UART_RX_CHAR_BUFFER] = {0};
-	uint8_t *ptr = message;
+    char str[64] = {0};
+    int errorCode = 0;
 
-	if(uartRxMsgBufferPointer.iCnt_Handle)
-	{
-		//copy buffer contents to array
-		memcpy(&message, &uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexOUT].data, sizeof(message));
+    if(uartRxMsgBufferPointer.iCnt_Handle){
+        memcpy(&message, &uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexOUT].data, sizeof(message));
 
-		// Example, if message was "get mcu fw version"
-		RemoveSpaces((char*)ptr);
-		// now message is "getmcufwversion"
-
-		// look for keyword "get"
-		if(strncmp((char*)message, "get", strlen("get")) == 0)
-		{
-			ptr += strlen("get"); // removes "get"
-			GetMessage(ptr); // pass message "mcufwversion"
-		}
-		// if message is "set led green: on"
-		else if(strncmp((char*)message, "set", strlen("set")) == 0)
-		{
-			ptr += strlen("set"); // removes "set"
-			SetMessage(ptr); // pass message "ledgreen:on"
-		}
-
-		DRV_RingBuffPtr__Output(&uartRxMsgBufferPointer, MAX_UART_RX_MESSAGE_BUFFER);
-	}
+        if(strncmp((char*)message, "CMD0", 4) == 0){
+            strcpy(str, "I received CMD0\r\n");
+            UartCopyStrToCharBufferTxStruct(UART_PORT_1 , &uartTx, str);
+            UartAddTxMessageBuffer(&uartTx);
+            TimerCallbackEnable(SendUart_Message, DISABLE);
+        }
+        else if(strncmp((char*)message, "CMD1", 4) == 0){
+            strcpy(str, "I received CMD1\r\n");
+            UartCopyStrToCharBufferTxStruct(UART_PORT_1 , &uartTx, str);
+            UartAddTxMessageBuffer(&uartTx);
+            TimerCallbackEnable(SendUart_Message, ENABLE);
+        }
+        else
+        {
+            errorCode = UartCopyStrToCharBufferTxStruct(UART_PORT_1 , &uartTx, (char*)message);
+            if(errorCode != 0){
+                return;
+            }
+            UartAddTxMessageBuffer(&uartTx);
+        }
+        memset(&uartRxMessageBuffer[uartRxMsgBufferPointer.iIndexOUT].data, 0, MAX_UART_RX_CHAR_BUFFER); // clear data
+        DRV_RingBuffPtr__Output(&uartRxMsgBufferPointer, MAX_UART_RX_MESSAGE_BUFFER);
+    }
 }
 */
 
--- UartCharBuffer.h
+++ UartCharBuffer.h
@@ -8,55 +8,89 @@
 #ifndef UARTBUFFER_H_
 #define UARTBUFFER_H_
 
+#include "main.h"
 
-#define MAX_UART_RX_CHAR_BUFFER_SINGLE 1 // this is for the uart IRQ array reference. For STM32 we are receiving one character at a time.
-#define MAX_UART_RX_CHAR_BUFFER 192
-#define MAX_UART_TX_CHAR_BUFFER 192
-#define MAX_UART_RX_MESSAGE_BUFFER 3
-#define MAX_UART_TX_MESSAGE_BUFFER 3
+enum
+{
+	UART_PORT_0,
+	UART_PORT_1,
+	UART_PORT_2,
+	UART_PORT_3,
+	UART_PORT_4,
+	UART_PORT_5,
+	UART_PORT_6,
+	UART_PORT_7
+};
 
 
-#ifndef ERRORSTATUS_H_
-#define NO_ERROR 0
-#define UART_BUSY 1
-#endif
+// buffer status
+enum
+{
+	UART_BUFFER_EMPTY,
+	UART_BUFFER_OVERFLOW
+};
+
+
+// User defines start
+/* 	MAX_UART_RX_IRQ_BYTE_LENGTH is for the UART IRQ. For strings that vary in length, this should be 1.
+ *	If packets with MOD256 checksum, then change to received packet size.
+ *	Currently this does not support varied size binary data with checksum, just fixed size only. In a future revision this will.
+ *
+ */
+#define MAX_UART_RX_IRQ_BYTE_LENGTH 1
+
+#define MAX_UART_RX_BYTE_BUFFER 192 // this holds all the IRQ data
+#define MAX_UART_TX_BYTE_BUFFER 192
+#define MAX_UART_RX_MESSAGE_BUFFER 3 // buffer size of complete strings or packets.
+#define MAX_UART_TX_MESSAGE_BUFFER 3 // buffer size of complete strings or packets.
+
+#define NUMBER_OF_UART_PORTS 2 // This is for how many UART ports to receive from.
+#define UART_INDEX_PTR_0 UART_PORT_2 // assign index pointer 0 the UART port used
+#define UART_INDEX_PTR_1 UART_PORT_6 // assign index pointer 1 the UART port used
+// #define UART_INDEX_PTR_3 UART_PORT_3 // add more if needed
+
+// end user defines
+
 
 
 
 /*
- * Because different MCU's uses Uart port defines differently, we're going to use generic uart port names.
- * For your MCU you will convert the generic port names to the actual instance name when writing to the specific uart port.
+ * Description: Rx byte buffer to save for the Uart IRQ data received.
  *
  */
-typedef struct{
+typedef struct
+{
+	uint8_t data[MAX_UART_RX_IRQ_BYTE_LENGTH];
+}UartRxByteBufferStruct;
+
+
+/*
+ * Because different MCU's uses UART port defines differently, we're going to use generic UART port number.
+ * For your MCU you will convert the generic port numbers to the actual instance name when writing to the specific UART port.
+ *
+ */
+typedef struct
+{
 	uint8_t uartPort;
-	uint8_t data[MAX_UART_RX_CHAR_BUFFER];
-	uint32_t dataLength;
-}UartCharBufferRxStruct;
+	uint8_t data[MAX_UART_RX_BYTE_BUFFER];
+	uint8_t dataSize; // used mainly for binary data
+}UartRxMsgBufferStruct;
 
-typedef struct{
+typedef struct
+{
 	uint8_t uartPort;
-	uint8_t data[MAX_UART_TX_CHAR_BUFFER];
-	uint32_t dataLength;
-}UartCharBufferTxStruct;
+	uint8_t data[MAX_UART_TX_BYTE_BUFFER];
+	uint8_t dataSize; // used mainly for binary data
+}UartTxMsgBufferStruct;
 
-#define UART_PORT_0 0
-#define UART_PORT_1 1
-#define UART_PORT_2 2
-#define UART_PORT_3 3
-#define UART_PORT_4 4
-#define UART_PORT_5 5
-#define UART_PORT_6 6
-#define UART_PORT_7 7
 
-void UartParseRxCharBuffer(void);
-void UartParseRxByteBuffer(void);
+void UartParseRx_CHAR_Buffer(uint8_t index);
+void UartParseRx_BINARY_Buffer(uint8_t index, uint32_t packetSize);
 
-void UartAddTxMessageBuffer(UartCharBufferTxStruct *uartBufferPointer);
+void UartAddTxMessageBuffer(UartTxMsgBufferStruct *uartBufferPointer);
 void UartSendMessage(void);
-int UartAddCharToBuffer(uint8_t uartPort, uint8_t *_char);
-int UartCopyStrToCharBufferTxStruct(uint8_t uartPort, char *str_IN, UartCharBufferTxStruct *uartTx_OUT);
-int UartCopyDataToDataBufferTxStruct(uint8_t uartPort, uint8_t *data_IN, uint32_t dataLen, UartCharBufferTxStruct *uartTx_OUT);
+int UartAddByteToBuffer(uint8_t uartPort, uint8_t *_char, uint32_t sizeOfData);
+int UartCopyStrToCharBufferTxStruct(uint8_t uartPort, UartTxMsgBufferStruct *uartTx_OUT, char *str);
 void UartClearRxCharBuffer(void);
 
 
