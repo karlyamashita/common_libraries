3d3f221f2be0c88a39775455c777d4142e4c04b7
Karl Yamashita <karlyamashita@gmail.com>
2023-03-06 00:41:41 -0800

    Rename Functions in TimerDelay.c

--- TimerDelay.c
+++ TimerDelay.c
@@ -1,7 +1,7 @@
 /*
  *  Description: This sets up a generic timer that you can register.
- *              Use TimerGetValue(<timer instance>) to get the certain timer instance value.
- *              You can test the return value to see if the value is >= to a certain value
+ *              Use TimerDelayGetValue(<timer instance>) to get that timer instance value.
+ *              You can test the return value to see if the value is >= to a certain value like 1000 for 1 second.
  *
  *
  *
@@ -11,7 +11,7 @@
 #include "TimerDelay.h"
 
 
-#define MAX_TIMERS 14 // increase if more timers are needed
+#define MAX_TIMERS 8 // increase if more timers are needed
 
 volatile uint32_t genericTimer[MAX_TIMERS] = {0}; // array that holds timer values for each timer instance
 volatile uint8_t timerInstance = 0; // timer instance
@@ -19,12 +19,14 @@ volatile uint8_t timerInstance = 0; // timer instance
 /*
 function: Declare an uint8_t variable in your code and save the pointer value called from this function.
 					This variable will be used to GetTimer or SetTimer
-					e.g. uint8_t myTimer = TimerRegister();
+					e.g. uint8_t myTimer = TimerDelayRegister();
+						TimerDelaySetValue(myTimer, 0);
 					Note: Currently there is no function to unregister a timer.
 input: none
-output: the new timer instance pointer
+output: none
+return: the new timer instance pointer
 */
-uint8_t TimerRegister(void) {
+uint8_t TimerDelayRegister(void) {
 	if(timerInstance == MAX_TIMERS) {
 			printf("Maximum timers reached!");
 		return 0; // added return 0; 10-10-17
@@ -36,10 +38,11 @@ uint8_t TimerRegister(void) {
 
 /*
 function: Call this function from SysTick_Handler() in stm32f1xx_it.c
+			SysTick is typically a 1ms tick.
 input: none
 output: none
 */
-void TimerSYSTICK(void) {
+void TimerDelaySYSTICK(void) {
 	uint8_t i;
 	if(timerInstance == 0) return;   // no timers create so return
 	for(i = 1; i < timerInstance + 1; i++) { // increment all timer instances
@@ -52,16 +55,16 @@ function: Call this function to return the timer value
 input: The timer instance
 output: none
 */
-uint32_t TimerGetValue(uint8_t timer) {
+uint32_t TimerDelayGetValue(uint8_t timer) {
     return genericTimer[timer];
 }
 
 /*
 function: Set the timer instance to a value, usually you would reset the value to zero.
-input: none
+input: The timer instance and a value
 output: none
 */
-void TimerSetValue(uint8_t timer, uint32_t value) {
+void TimerDelaySetValue(uint8_t timer, uint32_t value) {
     genericTimer[timer] = value;
 }
 
--- TimerDelay.h
+++ TimerDelay.h
@@ -3,11 +3,11 @@
 
 #include "main.h"
 
-uint8_t TimerRegister(void);
+uint8_t TimerDelayRegister(void);
 
-uint32_t TimerGetValue(uint8_t timer);
-void TimerSYSTICK(void);
-void TimerSetValue(uint8_t timer, uint32_t value);
+uint32_t TimerDelayGetValue(uint8_t timer);
+void TimerDelaySYSTICK(void);
+void TimerDelaySetValue(uint8_t timer, uint32_t value);
 
 
 #endif // TIMER_DELAY_H
