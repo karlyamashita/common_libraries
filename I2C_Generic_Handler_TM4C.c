fbb9b4fe74b0a7717155fe92bd5652186118a716
Karl Yamashita <karl.yamashita@advantest.com>
2024-11-19 14:58:36 -0800

    Finished I2C_Master_WriteRegister function

--- I2C_Generic_Handler_TM4C.c
+++ I2C_Generic_Handler_TM4C.c
@@ -4,24 +4,163 @@
  * 
  * 
  * 
- * Note for Microchip: You should have a timer tick routine of 1ms and a call TMR1_GetTick() to get the value.
- *                     MCC should have generated i2c{x}.h file(s) where x is the peripheral I2C number. For this project we have two I2C peripherals being used.
- * 
  */
 
 #include "main.h"
 
 
-int I2C_InitGeneric(I2C_GenericDef *i2c, uint32_t i2c_base, volatile uint8_t *data, uint8_t deviceAddress)
+/*
+ * Instructions: User should create a variable of I2C_GenericDef.
+ *              User should also create a data buffer and assign to dataPtr
+ *              Example below
+
+uint8_t tmp102_dataRx[3]; // REG and 2 data bytes. Increase if register address and register data is larger
+
+I2C_DataStruct tmp102_data_1 =
 {
-    // todo - verify arguments are not null
+    .i2c_base = I2C0_BASE, // Assign i2c instance
+    .slaveAddr = (uint8_t) (TMP102_SLAVE_ADDRESS_00 << 1), // be sure to left shift Slave address
+    .cs.port = CS1_GPIO_Port, // Assign PORT
+    .cs.pin = CS1_Pin, // Assign Port pin
+    .dataPtr = tmp102_dataRx, // Assign Rx/Tx data array
+    .timeout = 100, // used in HAL polling timeout
+};
 
-    i2c->i2c_base = i2c_base;
-    i2c->dataPtr = (uint8_t*)data;
-    i2c->deviceAddr = deviceAddress;
+ *
+ *
+ */
 
-    return 0;
+
+// Polling
+static int I2C_Master_Receive_Generic(I2C_GenericDef *i2c);
+static int I2C_Master_Transmit_Generic(I2C_GenericDef *i2c);
+static int I2C_Mem_Read_Generic(I2C_GenericDef *i2c);
+static int I2C_Mem_Write_Generic(I2C_GenericDef *i2c);
+// Interrupt
+static int I2C_Mem_Read_Generic_IT(I2C_GenericDef *i2c);
+
+/*
+ * Description: Calling this function will use specific I2C driver, Polling, Interrupt or DMA.
+ */
+int I2C_Master_Receive_Generic_Method(I2C_GenericDef *i2c)
+{
+    int status = NO_ERROR;
+
+    switch(i2c->transferType)
+    {
+        case I2C_POLLING:
+            status = I2C_Master_Receive_Generic(i2c);
+            if(i2c->RxISR)
+            {
+                i2c->RxISR(i2c); // There is no interrupt so we manually call the callback
+            }
+            break;
+        case I2C_INTERRUPT:
+            //status = I2C_Master_Receive_Generic_IT(i2c);
+            break;
+        case I2C_DMA:
+            //status = I2C_Master_Receive_Generic_DMA(i2c);
+            break;
+        default:
+            status = I2C_Master_Receive_Generic(i2c);
+            break;
+    }
+
+    return status;
 }
+
+/*
+ * Description: Calling this function will use specific I2C driver, Polling, Interrupt or DMA.
+ */
+int I2C_Master_Transmit_Generic_Method(I2C_GenericDef *i2c)
+{
+    int status = NO_ERROR;
+
+    switch(i2c->transferType)
+    {
+        case I2C_POLLING:
+            status = I2C_Master_Transmit_Generic(i2c);
+            if(i2c->TxISR)
+            {
+                i2c->TxISR(i2c); // There is no interrupt so we manually call the callback
+            }
+            break;
+        case I2C_INTERRUPT:
+            //status = I2C_Master_Transmit_Generic_IT(i2c);
+            break;
+        case I2C_DMA:
+            //status = I2C_Master_Transmit_Generic_DMA(i2c);
+            break;
+        default:
+            status = I2C_Master_Transmit_Generic(i2c);
+            break;
+    }
+
+    return status;
+}
+
+/*
+ * Description: Calling this function will use specific I2C driver, Polling, Interrupt or DMA.
+ */
+int I2C_Mem_Read_Generic_Method(I2C_GenericDef *i2c)
+{
+    int status = NO_ERROR;
+
+    switch(i2c->transferType)
+    {
+        case I2C_POLLING:
+            status = I2C_Mem_Read_Generic(i2c);
+            if(i2c->RxISR)
+            {
+                i2c->RxISR(i2c); // There is no interrupt so we manually call the callback
+            }
+            break;
+        case I2C_INTERRUPT:
+            status = I2C_Mem_Read_Generic_IT(i2c);
+            break;
+        case I2C_DMA:
+            //status = I2C_Mem_Read_Generic_DMA(i2c);
+            break;
+        default:
+            status = I2C_Mem_Read_Generic(i2c);
+            break;
+    }
+
+    return status;
+}
+
+/*
+ * Description: Calling this function will use specific I2C driver, Polling, Interrupt or DMA.
+ */
+int I2C_Mem_Write_Generic_Method(I2C_GenericDef *i2c)
+{
+    int status = NO_ERROR;
+
+    switch(i2c->transferType)
+    {
+        case I2C_POLLING:
+            status = I2C_Mem_Write_Generic(i2c);
+            if(i2c->TxISR)
+            {
+                i2c->TxISR(i2c); // There is no interrupt so we manually call the callback
+            }
+            break;
+        case I2C_INTERRUPT:
+            //status = I2C_Mem_Write_Generic_IT(i2c);
+            break;
+        case I2C_DMA:
+            //status = I2C_Mem_Write_Generic_DMA(i2c);
+            break;
+        default:
+            status = I2C_Mem_Write_Generic(i2c);
+            break;
+    }
+
+    return status;
+}
+
+
+// ****************** Polling ****************
 
 
 /*
@@ -29,11 +168,11 @@ int I2C_InitGeneric(I2C_GenericDef *i2c, uint32_t i2c_base, volatile uint8_t *da
  * 
  * 
  */
-int I2C_Master_Receive_Generic(I2C_GenericDef *i2c)
+static int I2C_Master_Receive_Generic(I2C_GenericDef *i2c)
 {
     int status = NO_ERROR;
 
-    status = I2C_Master_Receive(i2c->i2c_base, i2c->deviceAddr, i2c->dataPtr, i2c->dataSize);
+    status = I2C_Master_Receive(i2c->i2c_base, i2c->deviceAddr, i2c->dataPtr, i2c->dataSize, i2c->timeout);
     if(status != 0)
     {
         status = I2C_ERROR_READ;
@@ -47,11 +186,11 @@ int I2C_Master_Receive_Generic(I2C_GenericDef *i2c)
  * 
  * 
  */
-int I2C_Master_Transmit_Generic(I2C_GenericDef *i2c)
+static int I2C_Master_Transmit_Generic(I2C_GenericDef *i2c)
 {
     int status = NO_ERROR;
 
-    status = I2C_Master_Transmit(i2c->i2c_base, i2c->deviceAddr, i2c->dataPtr, i2c->dataSize);
+    status = I2C_Master_Transmit(i2c->i2c_base, i2c->deviceAddr, i2c->dataPtr, i2c->dataSize, i2c->timeout);
     if(status != 0)
     {
         status = I2C_ERROR_WRITE;
@@ -64,11 +203,11 @@ int I2C_Master_Transmit_Generic(I2C_GenericDef *i2c)
  * Description: Reads register data
  * 
  */
-int I2C_Mem_Read_Generic(I2C_GenericDef *i2c)
+static int I2C_Mem_Read_Generic(I2C_GenericDef *i2c)
 {  
     int status = NO_ERROR;
 
-    status = I2C_Master_ReadRegister(i2c->i2c_base, i2c->deviceAddr, i2c->registerAddr, i2c->regSize, i2c->dataPtr, i2c->dataSize, i2c->timeout);
+    status = I2C_Master_ReadRegister(i2c->i2c_base, i2c->deviceAddr, i2c->registerAddr[0], i2c->regSize, i2c->dataPtr, i2c->dataSize, i2c->timeout);
     if(status != 0)
     {
         status = I2C_ERROR_READ;
@@ -81,13 +220,30 @@ int I2C_Mem_Read_Generic(I2C_GenericDef *i2c)
  * Description: Writes register data
  *
  */
-int I2C_Mem_Write_Generic(I2C_GenericDef *i2c)
+static int I2C_Mem_Write_Generic(I2C_GenericDef *i2c)
 {
+    int status = NO_ERROR;
 
+    status = I2C_Master_WriteRegister(i2c->i2c_base, i2c->deviceAddr, i2c->registerAddr[0], i2c->regSize, i2c->dataPtr, i2c->dataSize, i2c->timeout);
+    if(status != 0)
+    {
+        status = I2C_ERROR_WRITE;
+    }
 
-    return I2C_Master_Transmit_Generic(i2c);
-
-
+    return status;
 }
 
+
+static int I2C_Mem_Read_Generic_IT(I2C_GenericDef *i2c)
+{
+    int status = NO_ERROR;
+
+    status = I2C_Master_MemReadRegister(i2c->i2c_base, i2c->deviceAddr, i2c->registerAddr[0], i2c->regSize, i2c->dataPtr, i2c->dataSize);
+    if(status != 0)
+    {
+        status = I2C_ERROR_READ;
+    }
+
+    return status;
+}
 
--- I2C_Generic_Handler_TM4C.h
+++ I2C_Generic_Handler_TM4C.h
@@ -12,45 +12,65 @@
 extern "C" {
 #endif
 
-enum I2C_TransferType
+
+enum TransferType_enum
 {
-    I2C_POLLING_TRANSFER,
-    I2C_INTERRUPT_TRANSFER,
-    I2C_DMA_TRANSFER
+    I2C_POLLING,
+    I2C_INTERRUPT,
+    I2C_DMA
 };
 
+
+#ifdef HAL_MODULE_ENABLED // STM32
 typedef struct
 {
+    uint32_t pin;
+    GPIO_TypeDef *port; // port base
+}GPIO_CS_t; // chip select
+#endif
+
+#ifndef XMEGA
+typedef struct __I2C_GenericDef_ // Microchip Studio compiler doesn't work with __I2C_GenericDef_
+#else
+typedef struct
+#endif
+{
+#ifdef XMEGA
+    TWI_t *twi_t;
+#endif
+#ifdef HAL_MODULE_ENABLED // STM32
+    I2C_HandleTypeDef *i2c_instance;
+#endif
+#ifdef TARGET_IS_TM4C123_RB1
     uint32_t i2c_base;
+#endif
 
     uint8_t deviceAddr; // the slave address of device
     uint8_t *dataPtr; // pointer to data array
-    uint16_t registerAddr;
+    uint8_t registerAddr[2]; // 8 or 16 bit address
     uint32_t dataSize;
-    uint32_t regSize;
-    uint32_t timeout;
-    uint32_t transferType;
-}I2C_GenericDef;
+    RING_BUFF_STRUCT dataIdxPtr;
+    uint32_t regSize; // the size of the registerAddr, 1 or 2 bytes
+    uint32_t timeout; // typically for STM32 that uses HAL timeout
+    uint32_t transferType; // I2C_POLLING, I2C_INTERRUPT, I2C_DMA
+    // TODO - define CS for TI and XMEGA
+#ifdef HAL_MODULE_ENABLED // STM32
+    GPIO_CS_t cs; // chip select
+#endif
+#ifndef XMEGA // all but XMEGA can use these callbacks
+    void (*RxISR)(struct __I2C_GenericDef_ *i2c);
+    void (*TxISR)(struct __I2C_GenericDef_ *i2c); // probably not needed but here nonetheless
+#endif
+    char *cmdPtr; // pointer to copy of command message
+}__attribute__((aligned(32))) I2C_GenericDef;
 
-int I2C_InitGeneric(I2C_GenericDef *i2c, uint32_t i2c_base, volatile uint8_t *data, uint8_t deviceAddress);
-    
-int I2C_Master_Receive_Generic(I2C_GenericDef *msg);
-int I2C_Master_Transmit_Generic(I2C_GenericDef *msg);
 
-int I2C_Master_Receive_Generic_IT(I2C_GenericDef *msg);
-int I2C_Master_Transmit_Generic_IT(I2C_GenericDef *msg);
+int I2C_Master_Receive_Generic_Method(I2C_GenericDef *i2c);
+int I2C_Master_Transmit_Generic_Method(I2C_GenericDef *i2c);
+int I2C_Mem_Read_Generic_Method(I2C_GenericDef *i2c);
+int I2C_Mem_Write_Generic_Method(I2C_GenericDef *i2c);
 
-int I2C_Master_Receive_Generi_DMA(I2C_GenericDef *msg);
-int I2C_Master_Transmit_Generic_DMA(I2C_GenericDef *msg);
 
-int I2C_Mem_Read_Generic(I2C_GenericDef *msg);
-int I2C_Mem_Write_Generic(I2C_GenericDef *msg);
-
-int I2C_Mem_Read_Generic_IT(I2C_GenericDef *msg);
-int I2C_Mem_Write_Generic_IT(I2C_GenericDef *msg);
-
-int I2C_Mem_Read_Generic_DMA(I2C_GenericDef *msg);
-int I2C_Mem_Write_Generic_DMA(I2C_GenericDef *msg);
 
 #ifdef	__cplusplus
 }
