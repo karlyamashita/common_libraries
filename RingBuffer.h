073b7a2c59b380d9087a4165ca896fae8d7c3300
Karl Yamashita <karl.yamashita@advantest.com>
2025-02-08 14:53:16 -0800

    added skip overflow

--- RingBuffer.h
+++ RingBuffer.h
@@ -10,6 +10,7 @@ typedef struct {
 	uint32_t index_OUT; // pointer to next available data in buffer if cnt_handle is not zero
 	uint32_t cnt_Handle; // if not zero then message available
 	uint32_t cnt_OverFlow; // has overflow if not zero
+	uint8_t SkipOverFlow;
 }RING_BUFF_STRUCT;
 
 void RingBuff_Ptr_Reset(RING_BUFF_STRUCT *ptr);
