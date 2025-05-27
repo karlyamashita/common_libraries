37741a6b1b70994c37a172e2bf58694edfb2d962
Karl Yamashita <karl.yamashita@advantest.com>
2025-05-27 14:50:28 -0700

    Change data structure

--- CAN_Filter.c
+++ CAN_Filter.c
@@ -9,8 +9,18 @@ function: Set CAN filter to pass all ID's
 input: hcan instance
 output: none
 */
-void CAN_SetFilter(CAN_HandleTypeDef *hcan) {
+void CAN_SetFilter(CAN_MsgStruct *msg)
+{
 	static CAN_FilterTypeDef sFilterConfig;
+
+	if(msg->hcan == &hcan1)
+	{
+		sFilterConfig.FilterBank = 0;
+	}
+	else if(msg->hcan == &hcan2)
+	{
+		sFilterConfig.FilterBank = 14;
+	}
 
 	sFilterConfig.FilterMode             = CAN_FILTERMODE_IDMASK;
 	sFilterConfig.FilterScale            = CAN_FILTERSCALE_32BIT;
@@ -21,15 +31,15 @@ void CAN_SetFilter(CAN_HandleTypeDef *hcan) {
 	sFilterConfig.FilterFIFOAssignment   = CAN_FILTER_FIFO0;
 	sFilterConfig.FilterActivation       = ENABLE;
 
-	if(HAL_CAN_ConfigFilter(hcan, &sFilterConfig) != HAL_OK)
+	if(HAL_CAN_ConfigFilter(msg->hcan, &sFilterConfig) != HAL_OK)
 	{
 		Error_Handler(); // Error_Handler should never be called, so we won't worry about handling these errors.
 	}
-	if(HAL_CAN_Start(hcan) != HAL_OK)
+	if(HAL_CAN_Start(msg->hcan) != HAL_OK)
 	{
 		Error_Handler();
 	}
-	if (HAL_CAN_ActivateNotification(hcan, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK) // enables CAN notification. This is not the same as enabling CAN reception.
+	if (HAL_CAN_ActivateNotification(msg->hcan, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK) // enables CAN notification. This is not the same as enabling CAN reception.
 	{
 		Error_Handler();
 	}
--- CAN_Filter.h
+++ CAN_Filter.h
@@ -2,7 +2,7 @@
 #ifndef CAN_FILTER_H
 #define CAN_FILTER_H
 
-void CAN_SetFilter(CAN_HandleTypeDef *hcan);
+void CAN_SetFilter(CAN_MsgStruct *msg);
 
 #endif // CAN_FILTER_H
 
